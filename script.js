214a06cdf55ae15c8f7db761c0f677a87a809c81
Ninja <muhamadusulaiman41912@gmail.com>
2023-10-27 07:22:18 -0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById("email-link").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default link behavior
-    window.location.href = "mailto:muhamadusulaiman41912@gmail.com";
-  });
-  
+  event.preventDefault(); // Prevent the default link behavior
+  window.location.href = "mailto:muhamadusulaiman41912@gmail.com";
+});
