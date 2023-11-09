1c9a9b83e270d235dd5e7c0fa59e99435444481e
Ninja <muhamadusulaiman41912@gmail.com>
2023-11-09 06:36:40 -0800

    update

---
+++ art.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const boxes = document.querySelectorAll(".section");
+
+    function checkBoxesInView() {
+        boxes.forEach((box) => {
+            const rect = box.getBoundingClientRect();
+            const isInView = rect.top <= window.innerHeight * 0.75 && rect.bottom >= 0;
+
+            if (isInView) {
+                box.classList.add("active");
+            } else {
+                box.classList.remove("active");
+            }
+        });
+    }
+
+    // Initial check
+    checkBoxesInView();
+
+    // Check on scroll
+    document.addEventListener("scroll", checkBoxesInView);
+});
+
+
